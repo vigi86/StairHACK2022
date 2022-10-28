cec2631bc4a9cbdc913e1b0cb58bb656c484ab6c
kagifret <ilya.kruchenetskiy.01@stud.hslu.ch>
2022-10-28 23:47:15 +0200

    spider improvements in output

--- src/spider_test.py
+++ src/spider_test.py
@@ -17,14 +17,25 @@ else:
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
 
-http_encoding = r.encoding if 'charset' in r.headers.get('content-type', '').lower() else None
 
-for link in soup.find_all('a', href=True):
-    print(link['href'])
+con_article = soup.find(class_="component__item-wrapper")
+
+article_list = con_article.find_all(class_='item__anchor')
+
+
+for con_link in con_article.find_all('a'):
+    #link_name = con_article.find('a') #.text.split('/')
+    
+    print(con_link.get('href'))
+    
+
+
+#for link in soup.find_all('a', href=True):
+#    print(link['href'])
         
     
 # searches only specific link names
-#
+
 # for a in soup.findAll('a'):
 # if 'word' in a['href']:
 #    print 'found a url with 'word' in the link'
