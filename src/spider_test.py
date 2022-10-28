6f7b04bec1e5432d785b977e22ad87ef12737a91
kagifret <ilya.kruchenetskiy.01@stud.hslu.ch>
2022-10-28 23:20:57 +0200

    Added pipreqs txt

--- src/spider_test.py
+++ src/spider_test.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-
+#inpit url
 inString = input("What are we looking for? (Health (H) or Science (S))")
+#stores url
 url = 'https://www.example.com/'
 
+#red or blue pill
 if(inString) == "H":
     url = 'https://www.cbsnews.com/health/'
 if(inString) == "S":
@@ -11,16 +13,18 @@ if(inString) == "S":
 else:
     print("nah")
 
-
+#scoops the url contents
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
-
-# for link in soup.find_all('a'):
-#    print(link.get('href'))
 
 http_encoding = r.encoding if 'charset' in r.headers.get('content-type', '').lower() else None
 
 for link in soup.find_all('a', href=True):
     print(link['href'])
-
-# print(r.content)
+        
+    
+# searches only specific link names
+#
+# for a in soup.findAll('a'):
+# if 'word' in a['href']:
+#    print 'found a url with 'word' in the link'
