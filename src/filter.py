4828078cc500cd45b7c64c7c65b237fef4f72b47
teshi24 <nadja.stadi@gmail.com>
2022-10-28 22:47:20 +0200

    separated filter and demo script for how to use the filters

--- src/filter.py
+++ src/filter.py
@@ -1,24 +1,6 @@
-from queue import Empty
-
-
-s = [
-    "This is an input with a specific keyword (schnabulieren)",
-    "This is an input without a specific keyword (schnab)"
-]
-
-def filterForStringsWithKeyword(inputs, keyword):
+def onlyStringsWithKeyword(inputs, keyword):
     return list(filter(lambda x: keyword in x, inputs))
 
 
-def filterForStringsWithoutKeyword(inputs, keyword):
+def onlyStringsWithoutKeyword(inputs, keyword):
     return list(filter(lambda x: keyword not in x, inputs))
-
-
-
-print(filterForStringsWithKeyword(s, "This"))
-print(filterForStringsWithKeyword(s, "schnabulieren"))
-print(filterForStringsWithKeyword(s, "hallo"))
-
-print(filterForStringsWithoutKeyword(s, "This"))
-print(filterForStringsWithoutKeyword(s, "schnabulieren"))
-print(filterForStringsWithoutKeyword(s, "hallo"))
