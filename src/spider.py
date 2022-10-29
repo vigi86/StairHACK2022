bcb1b61c9ed379021db33a3fa307f4774964a878
teshi24 <nadja.stadi@gmail.com>
2022-10-29 09:58:14 +0200

    added spider.getRelevantUrls incl. demo

--- src/spider.py
+++ src/spider.py
@@ -2,12 +2,21 @@ import requests
 import filter
 from bs4 import BeautifulSoup
 
-# TODO: feed with correct urls
-url = 'https://www.cbsnews.com/health/'
-
 def getSoupPerUrl(url):
     r = requests.get(url)
     return BeautifulSoup(r.content, 'html.parser')
+
+# soup:             BeautifulSoup() source of urls
+# interestedIn:     list(str)       topics / keywords which MUST be in the url (ignored if empty)
+# notInterestedIn:  list(str)       topics / keywords which MUST NOT be in the url (ignored if empty)
+# return:           list(str)       filtered url list
+def getRelevantUrls(soup, interestedIn, notInterestedIn):
+    urls = map(lambda x: x['href'], soup.find_all('a', href=True))
+    if interestedIn != []:
+        urls = filter.findStringsContainingKeyword(urls, interestedIn)
+    if notInterestedIn != []:
+        urls = filter.findStringsNotContainingKeywords(urls, notInterestedIn)
+    return urls
 
 def readCbsNewsPage(soup):
     con_article = soup.find(class_="component__item-wrapper")
@@ -18,6 +27,3 @@ def readCbsNewsPage(soup):
         #link_name = con_article.find('a') #.text.split('/')
         print(con_link.get('href'))
         
-
-        
-print(getSoupPerUrl(url))
