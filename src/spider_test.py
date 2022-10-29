7f6cc2a3056c117312a365da8cdee5a1135b6079
Victor Fernandez <vigi86@gmail.com>
2022-10-29 10:09:46 +0200

    Overall optimization

--- src/spider_test.py
+++ src/spider_test.py
@@ -5,58 +5,55 @@ from bs4 import BeautifulSoup
 print()
 print()
 
-inString = input("What are we looking for? (Health (H) or Science (S)) =  ")
-
-print()
-print()
+inString = ''
 
 #stores url
 url = 'https://www.example.com/'
 
-#red or blue pill
-if(inString) == "H":
-    url = 'https://www.cbsnews.com/health/'
-if(inString) == "S":
-    url = 'https://www.cbsnews.com/science/'
-else:
-    print("nah")
+while(inString != 'quit'):
+    #red or blue pill
+    inString = input("What are we looking for?  ").lower()
+    url = f"https://www.cbsnews.com/{inString}/"
 
-#scoops the url contents
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
+    #scoops the url contents
+    httpResponse = requests.get(url)
+    soup = BeautifulSoup(httpResponse.content, 'html.parser')
 
 
-con_article = soup.find(class_="component__item-wrapper")
+    con_article = soup.find(class_="component__item-wrapper")
 
-#article_list = con_article.find_all(class_='item__anchor')
+    #article_list = con_article.find_all(class_='item__anchor')
 
 
-for con_link in con_article.find_all('a'):
-    #print(con_link.get('href'))
-    use_link = con_link.get('href')
-    
     try:
-        r = requests.get(use_link)
-        soup = BeautifulSoup(r.content, 'html.parser')
-    
-        link_article = soup.find(class_="content__body")   
-        
-        print()
-        print()
-    
-        for link_quote in link_article.find('p'):
-            print(link_quote.getText())
-            
-    except:
-        print()
-        print()
-        print("error")     
-    
-    
-        
-    
-            
-    
+        for con_link in con_article.find_all('a'):
+            #print(con_link.get('href'))
+            use_link = con_link.get('href')
+
+            try:
+                httpResponse = requests.get(use_link)
+                soup = BeautifulSoup(httpResponse.content, 'html.parser')
+
+                link_article = soup.find(class_="content__body")
+
+                text = ''
+                for link_quote in link_article.find('p'):
+                    text += link_quote.getText()
+
+                print(text, end='\n\n\n')
+
+            except:
+                # do nothing and continue fetch
+                var = 0
+
+    except AttributeError:
+        print("Section not found. Try something else.", end='\n\n\n')
+
+
+
+
+
+
 # searches only specific link names
 
 # for a in soup.findAll('a'):
