e7c199b42ab0f4a35cdf1078228ab650f72a0814
kagifret <ilya.kruchenetskiy.01@stud.hslu.ch>
2022-10-29 15:59:00 +0200

    needs more fixes

---
+++ src/spider_abcnews_test.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+#inpit url
+
+print()
+print()
+
+inString = ''
+
+#stores url
+url = 'https://www.example.com/'
+
+while(inString != 'quit'):
+    #red or blue pill
+    inString = input("What are we looking for?  ").lower()
+    url = f"https://abcnews.go.com/{inString}/"
+
+    #scoops the url contents
+    httpResponse = requests.get(url)
+    soup = BeautifulSoup(httpResponse.content, 'html.parser')
+
+    con_article = soup.find(class_="ContentRoll")
+
+    try:
+        for con_link in con_article.find_all('a'):
+            #print(con_link.get('href'))
+            use_link = con_link.get('href')
+
+            try:
+                httpResponse = requests.get(use_link)
+                soup = BeautifulSoup(httpResponse.content, 'html.parser')
+
+                link_article = soup.find('div', attrs={"data-testid": 'prism-headline'})
+
+                # text = ''
+                # for link_quote in link_article.find('p'):
+                #     text += link_quote.getText()
+
+                # print(text, end='\n\n\n')
+                
+                print(link_article)
+
+            except:
+                # do nothing and continue fetch
+                var = 0
+
+    except AttributeError:
+        print("Section not found. Try something else.", end='\n\n\n')
+
+
+# searches only specific link names
+
+# for a in soup.findAll('a'):
+# if 'word' in a['href']:
+#    print 'found a url with 'word' in the link'
