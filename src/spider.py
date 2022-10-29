500552a30004111e712132c2d1e3b7572426f281
teshi24 <nadja.stadi@gmail.com>
2022-10-29 09:35:11 +0200

    & C:/Python310/python.exe d:/HackStair/StairHACK2022/src/spider.py

--- src/spider.py
+++ src/spider.py
@@ -1,22 +1,23 @@
 import requests
+import filter
 from bs4 import BeautifulSoup
 
+# TODO: feed with correct urls
+url = 'https://www.cbsnews.com/health/'
 
-#input???
+def getSoupPerUrl(url):
+    r = requests.get(url)
+    return BeautifulSoup(r.content, 'html.parser')
 
-#feed with urls
-url = ''
+def readCbsNewsPage(soup):
+    con_article = soup.find(class_="component__item-wrapper")
 
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
+    article_list = con_article.find_all(class_='item__anchor')
 
-#cbsnews
-con_article = soup.find(class_="component__item-wrapper")
+    for con_link in con_article.find_all('a'):
+        #link_name = con_article.find('a') #.text.split('/')
+        print(con_link.get('href'))
+        
 
-article_list = con_article.find_all(class_='item__anchor')
-
-
-for con_link in con_article.find_all('a'):
-    #link_name = con_article.find('a') #.text.split('/')
-    
-    print(con_link.get('href'))
+        
+print(getSoupPerUrl(url))
