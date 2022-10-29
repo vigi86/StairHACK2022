d55e94a836eb1b150ebf8e55f4ba33cedf7b1982
domebuehler <domebuehler7@gmail.com>
2022-10-29 10:24:41 +0200

    fix typo

--- src/main.py
+++ src/main.py
@@ -8,12 +8,12 @@ inputType = Enum('inputType', ['LIKES', 'DISLIKES'])
 # maybe add asking for username
 print("Welcome to TrueNews")
 
-# get the users intrests
+# get the users interests
 print("What are you you interested in? (enter q to quit)")
 writeListToFile(getTheUsersInterests(), inputType.LIKES)
 
 # get what the user would rather not see
-print("What would you rather not see?")
+print("What would you rather not see? (enter q to quit)")
 writeListToFile(getTheUsersInterests(), inputType.DISLIKES)
 
 print("----------------------------------END-------------------------------------")
