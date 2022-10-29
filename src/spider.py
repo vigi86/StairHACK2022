5926fe793fc495f415343b1028eb7d609c4c72f8
kagifret <ilya.kruchenetskiy.01@stud.hslu.ch>
2022-10-29 15:59:14 +0200

    abcnews source

--- src/spider.py
+++ src/spider.py
@@ -53,3 +53,11 @@ def readCbsNewsPage(soup):
         #link_name = con_article.find('a') #.text.split('/')
         print(con_link.get('href'))
         
+def readAbcNewsPage(soup):
+    con_article = soup.find(class_="component__item-wrapper")
+    
+    article_list = con_article.find_all(class_='item__anchor')
+    
+    for con_link in con_article.find_all('a'):
+        #link_name = con_article.find('a') #.text.split('/')
+        print(con_link.get('href'))
